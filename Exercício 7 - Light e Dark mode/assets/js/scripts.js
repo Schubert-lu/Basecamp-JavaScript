36e117fe3fc614d8290f268e378c0b5ba2e0e8ea
Schubert-lu <lucas.gomes_2012@hotmail.com>
2022-04-23 22:02:39 -0300

    Adicionando o exercício 7

---
+++ Exercício 7 - Light e Dark mode/assets/js/scripts.js
@@ -0,0 +1,36 @@
+
+const darkModeClass = 'dark-mode';
+const botao = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+
+function changeMode(){
+    changeClasses();
+    changeText();
+}
+
+function changeClasses(){
+    botao.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+}
+
+function changeText(){
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+    
+    if(body.classList.contains(darkModeClass)){
+        botao.innerHTML = lightMode;
+        h1.innerHTML = darkMode + " ON";
+        return;
+    }
+    botao.innerHTML = darkMode;
+    h1.innerHTML = lightMode + " ON";
+}
+
+botao.addEventListener('click', changeMode);
+
+console.log(botao)
